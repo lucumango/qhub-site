49df7d9801261c828e8cbaa59f2929f8ea02dcb1
lucumango <idaniellavargas@gmail.com>
2025-07-24 22:54:24 -0500

    fix: team rows

--- src/pages/Equipo.tsx
+++ src/pages/Equipo.tsx
@@ -188,7 +188,7 @@ const Equipo = () => {
       {/* Team Grid */}
       <section className="py-20 relative z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-8">
             {teamMembers.map((member, index) => (
               <Card 
                 key={member.id}
