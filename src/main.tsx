123f6f0851e21361786fd593009463fe38a81548
dynngz <dayanagzrd@gmail.com>
2025-11-11 20:47:30 -0500

    feat: add MathJax and Modulo3

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import MathJaxProvider from '@/components/MathRenderer'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+	<MathJaxProvider>
+		<App />
+	</MathJaxProvider>
+);
