823affd7bfa4b9a7a087963fd2ec35f19d7679be
Enricito-IsHere <enriqueespinozadioses@gmail.com>
2026-01-24 20:10:14 -0500

    Añadido de credenciales de Supabase con funciones de Login y SignUp

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface VariEnv{
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta{
+    readonly env: VariEnv
+}
